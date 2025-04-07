8ba6eed18dfe90b2559baf0b5c4071bd7155c065
Petr Fifka <petrfifk@gmail.com>
2025-04-07 19:36:33 +0200

    add grep tags

--- cypress/e2e/pmtool_login_tests.cy.js
+++ cypress/e2e/pmtool_login_tests.cy.js
@@ -1,6 +1,6 @@
 //pmtool_login_tests.cy.js
 
-describe("PMtool Login Tests", () => {
+describe("PMtool Login Tests", { tags: "@smoke" }, () => {
   it("Title exist", () => {
     cy.visit("https://tredgate.com/pmtool");
     cy.get("h3.form-title").should("exist");
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+import registerCypressGrep from "@cypress/grep/src/support";
+registerCypressGrep();
