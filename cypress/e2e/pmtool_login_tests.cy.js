435ed1bb5b2e1fa171ee5c466b6f48ec97b712bc
Petr Fifka <petrfifk@gmail.com>
2025-04-07 19:04:45 +0200

    add pmtool login test

---
+++ cypress/e2e/pmtool_login_tests.cy.js
@@ -0,0 +1,8 @@
+//pmtool_login_tests.cy.js
+
+describe("PMtool Login Tests", () => {
+  it("Title exist", () => {
+    cy.visit("https://tredgate.com/pmtool");
+    cy.get("h3.form-title").should("exist");
+  });
+});
